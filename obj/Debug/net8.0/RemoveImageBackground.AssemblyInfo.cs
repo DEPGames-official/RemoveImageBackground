e09dc510231bf2127a31a32049c9077548516602
Xavier Tame <dreyespoppergames@gmail.com>
2024-07-07 16:47:54 +0200

    Updated images

--- obj/Debug/net8.0/RemoveImageBackground.AssemblyInfo.cs
+++ obj/Debug/net8.0/RemoveImageBackground.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RemoveImageBackground")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ba9c3f1ea465a2aec119222a83314db9fb2e5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b86aa27e3e257ca9ea531ada2fdd1b8333a8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RemoveImageBackground")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RemoveImageBackground")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
